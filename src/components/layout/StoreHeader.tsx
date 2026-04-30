5c4ebd9c87438f5b71af12f797e699a814623f60
Eduardo Farias Camargo <edu.farias.camargo@gmail.com>
2026-04-29 22:25:41 -0300

    refactor: move store header to layout

--- src/components/layout/StoreHeader.tsx
+++ src/components/layout/StoreHeader.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import techstoreIconUrl from "@/assets/techstore-icon.svg";
 
 type StoreHeaderProps = {
   onSignOut: () => void;
@@ -8,7 +9,8 @@ export function StoreHeader({ onSignOut }: StoreHeaderProps) {
   return (
     <header className="store-header">
       <Link className="store-brand" to="/home" aria-label="TechStore home">
-        TechStore
+        <img className="store-brand-icon" src={techstoreIconUrl} alt="" aria-hidden="true" />
+        <span>TechStore</span>
       </Link>
 
       <button className="header-action" type="button" onClick={onSignOut}>
